fb7c54d7a331572552c2fd88f6a79a5804ac6f92
Garv Gupta <128141524+garvg4278@users.noreply.github.com>
2025-02-25 12:16:12 +0530

    Update AutoboxingUnboxingExample.java

--- AutoboxingUnboxingExample.java
+++ AutoboxingUnboxingExample.java
@@ -2,11 +2,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AutoboxingUnboxingExample {
-    
+
     public static int calculateSum(List<Integer> numbers) {
         int sum = 0;
         for (Integer num : numbers) {
             sum += num;
+            System.out.println("Adding " + num + " to sum, current sum: " + sum);
         }
         return sum;
     }
@@ -14,13 +15,18 @@ public class AutoboxingUnboxingExample {
     public static List<Integer> parseStringToIntegers(String[] strNumbers) {
         List<Integer> numbers = new ArrayList<>();
         for (String str : strNumbers) {
-            numbers.add(Integer.parseInt(str));
+            try {
+                numbers.add(Integer.parseInt(str));
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number format: " + str);
+            }
         }
+        System.out.println("Parsed numbers: " + numbers);
         return numbers;
     }
 
     public static void main(String[] args) {
-        String[] strNumbers = {"10", "20", "30", "40", "50"};
+        String[] strNumbers = { "10", "20", "30", "40", "50" };
         List<Integer> numbers = parseStringToIntegers(strNumbers);
         int sum = calculateSum(numbers);
         System.out.println("Sum of numbers: " + sum);
